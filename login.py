6363e5214bd0bea7a1551f2531b706d9d7468a75
Diego Medina <diegomedina739@gmail.com>
2026-05-01 20:55:02 -0500

    git stash

--- login.py
+++ login.py
@@ -1,2 +1,13 @@
+from hellogit4 import saludar
+
+
 print("hola, esta es la nueva funcionalidad")
 print("hola estoy en la rama login")
+
+saludar()
+
+def suma(a,b):
+    c=a+b
+    print(c)
+    return c
+suma(5,6)
